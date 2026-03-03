9e3e8a7d4287aa6e9b9bc23bbc14fe6e4db26120
Mosallam Abdulrahman <mosallamabdulrahman@gmail.com>
2026-03-03 03:02:40 +0200

    edit paths

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 function Root() {
   const { toggleTheme } = useTheme();
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Mosalam">
       <App onToggleTheme={toggleTheme} />
     </BrowserRouter>
   );
@@ -17,5 +17,5 @@ function Root() {
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Root />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
